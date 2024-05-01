e379db4c162cbe61fb5e7b03eccc337bab7c1123
Igor <igorperez2019@outlook.com>
2024-05-01 18:15:14 -0300

    Adicionando Código

---
+++ Menu/Menu/Model/MenuItem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menu.Model
+{
+    public class MenuItem
+    {
+		private string? _name;
+		public string Name
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+
+		private double _price;
+		public double Price
+		{
+			get { return _price; }
+			set { _price = value; }
+		}
+
+		private string? _description;
+		public string Description
+		{
+			get { return _description; }
+			set { _description = value; }
+		}
+
+		private string _image;
+		public string Image
+		{
+			get { return _image; }
+			set { _image = value; }
+		}
+
+	}
+}
--- Menu/Menu/View/MainPage.xaml.cs
+++ Menu/Menu/View/MainPage.xaml.cs
@@ -1,10 +1,28 @@
+using System.Collections.ObjectModel;
+
 namespace Menu.View;
 
 public partial class MainPage : ContentPage
 {
+
+	private ObservableCollection<MenuItem> _MenuItensList;
+
+	public ObservableCollection<MenuItem> MenuItensList
+    {
+		get { return _MenuItensList; }
+		set { _MenuItensList = value; }
+	}
+
+
 	public MainPage()
 	{
 		InitializeComponent();
 
+		MenuItensList = new ObservableCollection<MenuItem>()
+		{
+			
+		};
+
+        BindingContext = this;
     }
 }
